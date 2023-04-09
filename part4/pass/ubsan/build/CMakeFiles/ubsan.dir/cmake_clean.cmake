1aa2a40b41a867ba4d36a2b967a4c5ae1271804b
Song Ziyi <song.ziyi1@northeastern.edu>
2023-04-09 00:18:41 -0400

    submit

---
+++ part4/pass/ubsan/build/CMakeFiles/ubsan.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ubsan.dir/ubsan.cpp.o"
+  "CMakeFiles/ubsan.dir/ubsan.cpp.o.d"
+  "libubsan.pdb"
+  "libubsan.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ubsan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
